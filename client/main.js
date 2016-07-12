32296b7c96113ea39ae719bb430c715994373dfc
dmitry <dmitry.kouznetsov@paxport.se>
2016-07-12 18:42:23 +0200

    fist commit

---
+++ client/main.js
@@ -0,0 +1,27 @@
+import { Template } from 'meteor/templating';
+// import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+import { Rides } from '../both/rides.js'
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  // this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  rides() {
+    return Rides.find({});
+  },
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
