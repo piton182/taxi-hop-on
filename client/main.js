7e1e7fdbfcbd38559e2d3e4b8f156024bb216eba
dmitry <dmitry.kouznetsov@paxport.se>
2016-07-14 17:01:44 +0200

    ride details and join ride

--- client/main.js
+++ client/main.js
@@ -1,23 +1,39 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import { Rides } from '../both/rides.js'
 
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
-
+  this.state = new ReactiveDict()
+  this.state.setDefault({
+    // selectedRide: {...},
+  });
 });
 
 Template.hello.helpers({
   rides() {
     return Rides.find({});
   },
+  selectedRide() {
+    const instance = Template.instance();
+    return instance.state.get('selectedRide');
+  }
 });
 
 Template.hello.events({
   'click .js-ride-join'(event, instance) {
     if (Meteor.user()) {
       const username = Meteor.user().emails[0].address;
-      Rides.update({_id: this._id}, {$set: {coriders: username}})
+
+      Rides.update(
+        {_id: instance.state.get('selectedRide')._id },
+        { $set: { coriders: username } }
+      );
     }
+  },
+  'click .ride-time'(event, instance) {
+    instance.state.set('selectedRide', this);
   }
 });
