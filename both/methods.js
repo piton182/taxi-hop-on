59e3f99ffea7382d347ab27d74a7c0e1a6dc92b0
dmitry <dmitry.kouznetsov@paxport.se>
2016-07-20 11:05:23 +0200

    methods.js

---
+++ both/methods.js
@@ -0,0 +1,29 @@
+import { Rides } from '../both/rides.js'
+
+Meteor.methods(
+  {
+    'rides.join'( { rideId } ) {
+      // TODO: validate args
+
+      if (!Meteor.user()) {
+        console.error('rides.join.noauth');
+        throw new Meteor.Error('rides.join.noauth', 'No auth');
+      }
+
+      if (Rides.find(rideId).coriders) {
+        console.error('rides.join.rideClosed');
+        throw new Meteor.Error('rides.join.rideClosed', 'Ride is already joined by someone else');
+      }
+
+      console.info('User <' + Meteor.user()._id + '> is joining ride <' + rideId + '>.');
+      const username = Meteor.user().emails[0].address;
+      Rides.update(
+        {_id: rideId },
+        { $set: { coriders: username } }
+      );
+    },
+    // ''() {
+    //
+    // },
+  }
+);
